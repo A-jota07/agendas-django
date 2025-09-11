edca725c99b923cd9556fabed5e33fe6ced7688a
Alexandre Junior <souzajunioralexandre340@gmail.com>
2025-09-11 10:57:02 -0400

    Criação de imagens, criação da Categoria utilizando a foreignKey, Model Meta Options, verbose_name e verbose_name_plural e campo owner

--- contact/admin.py
+++ contact/admin.py
@@ -9,7 +9,13 @@ class ContactAdmin(admin.ModelAdmin):
     ordering = '-id',
     # list_filter = 'created_date', 
     search_fields = 'id', 'firs_name', 'last_name',
-    list_per_page = 1
+    list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'firt_name', 'last_name',
+    # list_editable = 'firt_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
+    
