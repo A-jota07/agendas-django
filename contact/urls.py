85fda2a8460a6b5125b0bf6d591fee792f18ca00
Alexandre Junior <souzajunioralexandre340@gmail.com>
2025-09-17 10:49:39 -0400

    Criação da paginação, search e detalhes

--- contact/urls.py
+++ contact/urls.py
@@ -6,4 +6,12 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    # path('contact/create/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
+
 ]
