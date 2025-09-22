e5b5f3433b03ce4225e1743cbf5f9fe5cde0e57d
Alexandre Junior <souzajunioralexandre340@gmail.com>
2025-09-22 13:56:46 -0400

    criação do update, configurações para email e atualização das urls

--- contact/urls.py
+++ contact/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     path('search/', views.search, name='search'),
 
     # contact (CRUD)
-    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
-    # path('contact/create/', views.contact, name='contact'),
-    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
-    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
 
+
+    # path('contact/create/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
