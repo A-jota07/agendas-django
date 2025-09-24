07b88f967488a93ffe2b96ad09f6846cb4bd65da
Alexandre Junior <souzajunioralexandre340@gmail.com>
2025-09-24 11:11:32 -0400

    Corrigindo .gitignore e adicionando arquivos .py

---
+++ contact/forms.py
@@ -0,0 +1,83 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+from . import models
+
+
+
+class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+
+    class Meta:
+        model = models.Contact
+        fields = (
+                'first_name','last_name', 'phone',
+                'email','description', 'category',
+                'picture',
+            )      
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                    'Os nomes não podem ser igual',
+                    code="invalid"
+                )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == "ABC":
+            raise ValidationError(
+                'Não digite ABC neste campo',
+                code='invalid'
+            )
+
+        return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        min_length=3
+    )
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+        def clean_email(self):
+            email = self.clean_data.get('email')
+
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError('Este e-mail já existe.')
+                )
+
+            return email
